f3550b233586f1bf158ff7529f03da71f22f6c59
Aribunfi <arianna@pabis.de>
2023-02-06 15:26:30 +0100

    array nomi

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
+
+let members = ["Wayne Barnett", "Angela Caroll", "Walter Gordon" , "Angela Lopez", "Scott Estrada", "Barbara Ramos"];
+
+let occupation = ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"];
+
+let data = {
+    
+        "name": ["Wayne", "Angela", "Walter", "Angela", "Scott", "Barbara"],
+        "surname": ["Barnett"],
+   
+      
+}
+
+console.log(data.name);
+
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
