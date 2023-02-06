0b16213fbb1d76a964f0a543ca6521a9e73f1c44
Aribunfi <arianna@pabis.de>
2023-02-06 17:36:43 +0100

    a fatica una card

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let data = {
     
         "name": ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"],
         "occupation": ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"],
-        "photo": ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor", "walter-gordon-office-manager", "angela-lopez-social-media-manager", "scott-estrada-developer", "barbara-ramos-graphic-designer"]
+        "photo": ["img/wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor", "walter-gordon-office-manager", "angela-lopez-social-media-manager", "scott-estrada-developer", "barbara-ramos-graphic-designer"]
 
 }
 
@@ -17,14 +17,25 @@ console.log(data.name);
 console.log(data.occupation);
 console.log(data.photo);
 
+for (i=0; i < 6; i++) {
+
+
+
+}
 
 const cardTitle = document.getElementById('member-name');
 
-cardTitle.append(data.name)
+cardTitle.append(data.name[0]) 
 
 const cardSubtitle = document.getElementById('member-position');
 
-cardTitle.append(data.occupation)
+cardTitle.append(data.occupation[0])
+
+const cardImgTop = document.getElementById('member-face');
+cardImgTop.append(data.photo[0])
+
+
+
 
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
