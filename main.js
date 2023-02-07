4647ffa9e0892384d792b73cb5db159e20206bcb
Aribunfi <arianna@pabis.de>
2023-02-07 16:46:23 +0100

    tentativo di correzione comunque non funzionante

--- main.js
+++ main.js
@@ -1,73 +1,87 @@
+const card = document.getElementById("teamMember");
+
+
+
+
 let members =
-   [
-        {
-        "name" : "Wayne Barnett",
+       [
+              {
+                     "name": "Wayne Barnett",
 
-        "occupation" : "Founder & CEO" ,
+                     "occupation": "Founder & CEO",
 
-        "photo" : "img/wayne-barnett-founder-ceo.jpg"
-        } ,
-
-
-        {"name": "Angela Caroll",
-
-        "occupation": "Chief Editor",
-
-        "photo": "angela-caroll-chief-editor.jpg"
-       },
+                     "photo": "img/wayne-barnett-founder-ceo.jpg"
+              },
 
 
-       
-       {"name": "Walter Gordon",
+              {
+                     "name": "Angela Caroll",
 
-       "occupation":  "Office Manager",
+                     "occupation": "Chief Editor",
 
-       "photo": "walter-gordon-office-manager.jpg"
-      },
-
-
-      {"name": "Angela Lopez",
-
-      "occupation":  "Social Media Manager",
-
-      "photo": "angela-lopez-social-media-manager.jpg"
-     },
-
-
-     {"name": "Scott Estrada",
-
-     "occupation":  "Developer",
-
-     "photo": "scott-estrada-developer.jpg"
-    },
-
-
-    
-    {"name": "Barbara Ramos",
-
-    "occupation":  "Graphic Designer",
-
-    "photo": "barbara-ramos-graphic-designer.jpg"
-   }
-
- ];
-
- for (let key in members) {
-        console.log(members[key]);
- }
+                     "photo": "angela-caroll-chief-editor.jpg"
+              },
 
 
 
+              {
+                     "name": "Walter Gordon",
 
-// const cardTitle = document.getElementById('member-name');
+                     "occupation": "Office Manager",
 
-// cardTitle.append(members.name)
+                     "photo": "walter-gordon-office-manager.jpg"
+              },
 
-// const cardSubtitle = document.getElementById('member-position');
 
-// cardTitle.append(members.occupation)
+              {
+                     "name": "Angela Lopez",
 
-// const cardImgTop = document.getElementById('member-face');
-// cardImgTop.append(members.photo)
+                     "occupation": "Social Media Manager",
+
+                     "photo": "angela-lopez-social-media-manager.jpg"
+              },
+
+
+              {
+                     "name": "Scott Estrada",
+
+                     "occupation": "Developer",
+
+                     "photo": "scott-estrada-developer.jpg"
+              },
+
+
+
+              {
+                     "name": "Barbara Ramos",
+
+                     "occupation": "Graphic Designer",
+
+                     "photo": "barbara-ramos-graphic-designer.jpg"
+              }
+
+       ];
+
+for (const member of members) {
+       console.log(member.name);
+       console.log(member.occupation);
+       console.log(member.photo);
+
+
+       const cardHTML =
+              `
+<div class="card" style="width: 18rem;" id="teamMember">
+<div class="cardBody">
+    <img src=". . ." class="cardImgTop" id="member-face" alt="...">
+
+    <h5 class="cardTitle" id="member-name">${member.name}</h5>
+    <h6 class="cardSubtitle" id="member-position">${member.occupation}</h6>
+
+</div>
+</div>`;
+}
+
+
+card.innerHTML += cardHTML;
 
 
