8135511eccdc0c9e2c8d6371d33a8d23f9b0737f
Aribunfi <arianna@pabis.de>
2023-02-07 08:36:20 +0100

    correzione array

--- main.js
+++ main.js
@@ -1,39 +1,73 @@
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
 
-let members = ["Wayne Barnett", "Angela Caroll", "Walter Gordon" , "Angela Lopez", "Scott Estrada", "Barbara Ramos"];
+//let members = ["Wayne Barnett", "Angela Caroll", "Walter Gordon" , "Angela Lopez", "Scott Estrada", "Barbara Ramos"];
 
-let occupation = ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"];
+//let occupation = ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"];
+let members =
 
-let data = {
-    
-        "name": ["Wayne Barnett", "Angela Caroll", "Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos"],
-        "occupation": ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"],
-        "photo": ["img/wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor", "walter-gordon-office-manager", "angela-lopez-social-media-manager", "scott-estrada-developer", "barbara-ramos-graphic-designer"]
+   [
+        {
+        "name" : "Wayne Barnett",
 
-}
+        "occupation" : "Founder & CEO" ,
 
-console.log(data.name);
-console.log(data.occupation);
-console.log(data.photo);
+        "photo" : "img/wayne-barnett-founder-ceo.jpg"
 
-for (i=0; i < 6; i++) {
+        } ,
 
 
+        {"name": "Angela Caroll",
 
-}
+        "occupation": "Chief Editor",
 
-const cardTitle = document.getElementById('member-name');
+        "photo": "angela-caroll-chief-editor"
 
-cardTitle.append(data.name[0]) 
+       }
 
-const cardSubtitle = document.getElementById('member-position');
 
-cardTitle.append(data.occupation[0])
+ ];
 
-const cardImgTop = document.getElementById('member-face');
-cardImgTop.append(data.photo[0])
+ for (let key in members) {
+        console.log(members[key]);
+ }
 
+
+
+          //Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos" , ,  "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"], }],
+
+             //"walter-gordon-office-manager", "angela-lopez-social-media-manager", "scott-estrada-developer", "barbara-ramos-graphic-designer"]
+
+
+
+// console.log(data.name);
+// console.log(data.occupation);
+// console.log(data.photo);
+
+
+
+// const cardTitle = document.getElementById('member-name');
+
+// cardTitle.append(members.name[0])
+
+// const cardSubtitle = document.getElementById('member-position');
+
+// cardTitle.append(members.occupation[0])
+
+// const cardImgTop = document.getElementById('member-face');
+// cardImgTop.append(members.photo[0])
+
+
+// for (let name in data) {
+//   console.log(data[name]);
+//  }
+
+// for (i=0; i < 6; i++) {
+
+//     document.createElement(data.name[members]);
+//     document.createElement(data.occupation[occupation]);
+
+// }
 
 
 
@@ -41,3 +75,12 @@ cardImgTop.append(data.photo[0])
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// const palla = {
+//         "prezzo": 12,
+//         "tipo": "pallina da golf"
+// };
+
+// for (let key in palla) {
+//         console.log(palla[key]);
+// }
