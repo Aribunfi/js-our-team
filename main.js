00634b50c85749cc175658487549515532a8e6af
Aribunfi <arianna@pabis.de>
2023-02-07 09:28:46 +0100

    sono arrivata fino a qui

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-// MILESTONE 0:
-// Creare l’array di oggetti con le informazioni fornite.
-
-//let members = ["Wayne Barnett", "Angela Caroll", "Walter Gordon" , "Angela Lopez", "Scott Estrada", "Barbara Ramos"];
-
-//let occupation = ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"];
 let members =
-
    [
         {
         "name" : "Wayne Barnett",
@@ -13,7 +6,6 @@ let members =
         "occupation" : "Founder & CEO" ,
 
         "photo" : "img/wayne-barnett-founder-ceo.jpg"
-
         } ,
 
 
@@ -22,7 +14,6 @@ let members =
         "occupation": "Chief Editor",
 
         "photo": "angela-caroll-chief-editor.jpg"
-
        },
 
 
@@ -32,7 +23,6 @@ let members =
        "occupation":  "Office Manager",
 
        "photo": "walter-gordon-office-manager.jpg"
-
       },
 
 
@@ -41,7 +31,6 @@ let members =
       "occupation":  "Social Media Manager",
 
       "photo": "angela-lopez-social-media-manager.jpg"
-
      },
 
 
@@ -50,7 +39,6 @@ let members =
      "occupation":  "Developer",
 
      "photo": "scott-estrada-developer.jpg"
-
     },
 
 
@@ -62,7 +50,6 @@ let members =
     "photo": "barbara-ramos-graphic-designer.jpg"
    }
 
-
  ];
 
  for (let key in members) {
@@ -72,49 +59,15 @@ let members =
 
 
 
-
-
-// console.log(data.name);
-// console.log(data.occupation);
-// console.log(data.photo);
-
-
-
 // const cardTitle = document.getElementById('member-name');
 
-// cardTitle.append(members.name[0])
+// cardTitle.append(members.name)
 
 // const cardSubtitle = document.getElementById('member-position');
 
-// cardTitle.append(members.occupation[0])
+// cardTitle.append(members.occupation)
 
 // const cardImgTop = document.getElementById('member-face');
-// cardImgTop.append(members.photo[0])
+// cardImgTop.append(members.photo)
 
 
-// for (let name in data) {
-//   console.log(data[name]);
-//  }
-
-// for (i=0; i < 6; i++) {
-
-//     document.createElement(data.name[members]);
-//     document.createElement(data.occupation[occupation]);
-
-// }
-
-
-
-// MILESTONE 1:
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-// MILESTONE 2:
-// Stampare le stesse informazioni su DOM sottoforma di stringhe
-
-// const palla = {
-//         "prezzo": 12,
-//         "tipo": "pallina da golf"
-// };
-
-// for (let key in palla) {
-//         console.log(palla[key]);
-// }
