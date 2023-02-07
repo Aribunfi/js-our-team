11a81302c3f5f0a230d57c02bb41789d858bc237
Aribunfi <arianna@pabis.de>
2023-02-07 08:50:38 +0100

    array completo

--- main.js
+++ main.js
@@ -21,9 +21,46 @@ let members =
 
         "occupation": "Chief Editor",
 
-        "photo": "angela-caroll-chief-editor"
+        "photo": "angela-caroll-chief-editor.jpg"
 
-       }
+       },
+
+
+       
+       {"name": "Walter Gordon",
+
+       "occupation":  "Office Manager",
+
+       "photo": "walter-gordon-office-manager.jpg"
+
+      },
+
+
+      {"name": "Angela Lopez",
+
+      "occupation":  "Social Media Manager",
+
+      "photo": "angela-lopez-social-media-manager.jpg"
+
+     },
+
+
+     {"name": "Scott Estrada",
+
+     "occupation":  "Developer",
+
+     "photo": "scott-estrada-developer.jpg"
+
+    },
+
+
+    
+    {"name": "Barbara Ramos",
+
+    "occupation":  "Graphic Designer",
+
+    "photo": "barbara-ramos-graphic-designer.jpg"
+   }
 
 
  ];
@@ -34,9 +71,6 @@ let members =
 
 
 
-          //Walter Gordon", "Angela Lopez", "Scott Estrada", "Barbara Ramos" , ,  "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"], }],
-
-             //"walter-gordon-office-manager", "angela-lopez-social-media-manager", "scott-estrada-developer", "barbara-ramos-graphic-designer"]
 
 
 
